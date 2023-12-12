a90d2fb3bb36e1b17ea1e5cb67fce84eb62317c6
Andrija Dumancic <andrija.dumancic@fer.hr>
2023-12-12 16:20:46 +0100

    Add TomTom SDK dependencies and Map component

--- lopta-app/src/components/JourneyRoute.jsx
+++ lopta-app/src/components/JourneyRoute.jsx
@@ -1,80 +1,86 @@
 import { FaLocationDot } from "react-icons/fa6";
 import { Tb123 } from "react-icons/tb";
 import { FaTransgender } from "react-icons/fa6";
+import Map from "./Map";
 
 const JourneyRoute = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div>
-        <h1 className="mb-4 font-bold text-2xl text-green-700">Please fill out information about your journey route.</h1>
-      </div>
+    <>
+      <Map />
+      <div className="flex flex-col items-center justify-center h-screen">
 
-      <div className="w-full max-w-2xl">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="flex items-center justify-between">
-            <div className="mb-4 py-2 px-2 w-full">
-              <label className="block text-gray-700 text-sm font-bold mb-2 flex items-center" htmlFor="starting address">
-                <FaLocationDot />
-                Starting address:
+        <div>
+          <h1 className="mb-4 font-bold text-2xl text-green-700">Please fill out information about your journey route.</h1>
+        </div>
+
+        <div className="w-full max-w-2xl">
+
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="flex items-center justify-between">
+              <div className="mb-4 py-2 px-2 w-full">
+                <label className="block text-gray-700 text-sm font-bold mb-2 flex items-center" htmlFor="starting address">
+                  <FaLocationDot />
+                  Starting address:
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="startingAddress"
+                  type="text"
+                  placeholder="Starting address"
+                />
+              </div>
+              <div className="mb-4 py-2 px-2 w-full">
+                <label className="block text-gray-700 text-sm font-bold mb-2 flex items-center" htmlFor="destination address">
+                  <FaLocationDot />
+                  Destination address:
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="destinationAddress"
+                  type="text"
+                  placeholder="Destination address"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2 flex items-center" htmlFor="age">
+                <Tb123 />
+                Age:
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="startingAddress"
-                type="text"
-                placeholder="Starting address"
+                className="shadow appearance-none border rounded w-20 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="age"
+                type="number"
+                placeholder="Age"
               />
             </div>
-            <div className="mb-4 py-2 px-2 w-full">
-              <label className="block text-gray-700 text-sm font-bold mb-2 flex items-center" htmlFor="destination address">
-                <FaLocationDot />
-                Destination address:
+
+            <div className="flex flex-col items-center justify-center mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2 flex items-center" htmlFor="gender">
+                <FaTransgender />
+                Gender:
               </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="destinationAddress"
-                type="text"
-                placeholder="Destination address"
-              />
+              <select id="gender" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-green-500 focus:border-green-500 block w-28 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
+                <option selected>Select a gender</option>
+                <option value="M">Male</option>
+                <option value="F">Female</option>
+                <option value="O">Other</option>
+              </select>
             </div>
-          </div>
 
-          <div className="flex flex-col items-center justify-center mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 flex items-center" htmlFor="age">
-              <Tb123 />
-              Age:
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-20 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="age"
-              type="number"
-              placeholder="Age"
-            />
-          </div>
-
-          <div className="flex flex-col items-center justify-center mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 flex items-center" htmlFor="gender">
-              <FaTransgender />
-              Gender:
-            </label>
-            <select id="gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-green-500 focus:border-green-500 block w-28 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
-              <option selected>Select a gender</option>
-              <option value="M">Male</option>
-              <option value="F">Female</option>
-              <option value="O">Other</option>
-            </select>
-          </div>
-
-          <div className="flex items-center justify-center">
-            <button
-              className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
-              Calculate
-            </button>
-          </div>
-        </form>
+            <div className="flex items-center justify-center">
+              <button
+                className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Calculate
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ lopta-app/src/components/Map.jsx
@@ -0,0 +1,156 @@
+import '@tomtom-international/web-sdk-maps/dist/maps.css'; // Import the CSS styles for the maps
+import { useState, useEffect, useRef } from "react";
+import tt from '@tomtom-international/web-sdk-maps'; // Import the TomTom Maps SDK
+import ad from '@tomtom-international/web-sdk-services'; // Import the TomTom Maps SDK
+
+
+function Map() {
+    const mapElement = useRef();
+    const [mapLongitude, setMapLongitude] = useState(-0.118092 || 0); // Providing a default value of 0 if null is encountered
+    const [mapLatitude, setMapLatitude] = useState(51.50000 || 0); // Providing a default value of 0 if null is encountered
+    const [map, setMap] = useState(null);
+    const [markers, setMarker] = useState([]);
+    const [allPoints, setAllPoints] = useState([]);
+
+    const updateLongitude = (value) => {
+        setMapLongitude((value));
+
+    };
+
+    const updateLatitude = (value) => {
+
+        setMapLatitude((value));
+
+    };
+
+    const updateMap = () => {
+        map.setCenter([parseFloat(mapLongitude), parseFloat(mapLatitude)]);
+        map.setZoom(10);
+    };
+
+    // ... (previous code remains the same)
+
+    useEffect(() => {
+        const initializeMap = async () => {
+            try {
+                console.log("Initializing map...");
+                const mapInstance = tt.map({
+                    key: "aLgQNoPtQzJe5nGzbNocRvlSyQEjlOF4",
+                    container: mapElement.current,
+                    center: [mapLongitude, mapLatitude],
+                    zoom: 10
+                });
+
+                if (mapInstance) {
+                    console.log("Map instance created:", mapInstance);
+                    setMap(mapInstance);
+                } else {
+                    console.error("Map instance not created");
+                }
+            } catch (error) {
+                console.error("Map initialization error:", error);
+            }
+        };
+
+        if (mapElement.current) {
+            initializeMap();
+        } else {
+            console.warn("Map element not found");
+        }
+
+        return () => {
+            if (map) {
+                map.remove();
+            }
+        };
+    }, [mapLongitude, mapLatitude]);
+
+
+
+    useEffect(() => {
+        if (map) {
+            const clickHandler = function (e) {
+                setMarker(prevMarkers => {
+                    const newMarker = new tt.Marker().setLngLat(e.lngLat).addTo(map);
+                    console.log(newMarker);
+                    const updatedMarkers = [...prevMarkers, newMarker];
+                    console.log("Updated markers are: ", updatedMarkers);
+                    return updatedMarkers;
+                });
+            };
+
+            map.on('click', clickHandler);
+
+            return () => {
+                map.off('click', clickHandler); // Clean up the event listener
+            };
+        }
+    }, [map]);
+
+
+    const displayRoute = (geoJson) => {
+        map.addLayer({
+            'id': 'route',
+            'type': 'line',
+            'source': {
+                'type': 'geojson',
+                'data': geoJson
+            },
+            'paint': {
+                'line-color': '#4a90e2',
+                'line-width': 6
+            }
+        })
+
+    }
+
+    const createRoute = async () => {
+        try {
+            if (markers.length === 0) {
+                console.log("No markers to create a route");
+                return;
+            }
+
+            const routeOptions = {
+                key: 'aLgQNoPtQzJe5nGzbNocRvlSyQEjlOF4',
+                locations: markers.map((marker) => marker.getLngLat()),
+                travelMode: 'car'
+            };
+
+            const routeData = await ad.services.calculateRoute(routeOptions);
+            if (routeData) {
+                console.log(routeData);
+                const geoJson = routeData.toGeoJson();
+                displayRoute(geoJson);
+                // Handle the route data or state update here
+                const allPointsData = [];
+                routeData.routes[0].legs[0].points.forEach(point => {
+                    allPointsData.push(point);
+                });
+                console.log(allPointsData);
+                setAllPoints(allPointsData);
+            } else {
+                console.error("Error calculating route: Invalid route data");
+            }
+        } catch (error) {
+            console.error("Error calculating route:", error);
+        }
+    };
+
+    return (
+        <div className="Map">
+
+            <div className="pt-10 container w-full h-max flex justify-center items-center">
+                <div className="w-1/2 h-56 shadow-2xl flex flex-col justify-center items-center">
+                    <div ref={mapElement} className="mapDiv w-full h-full" />
+                    <button onClick={createRoute} className="bg-blue-400 mt-4 px-4 py-2 text-white rounded-md">
+                        Create Route
+                    </button>
+                </div>
+            </div>
+
+        </div >
+    );
+}
+
+export default Map;
