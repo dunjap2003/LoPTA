95f7cb60f42ce37a357fafd1c3a4abde93754884
dunjap2003 <dunja.petrovic12@yahoo.com>
2024-01-05 18:24:36 +0100

    analysis added

---
+++ lopta-app/src/Data.js
@@ -0,0 +1,709 @@
+export const Data = [
+    {
+        day: "Sunday",
+        hourResults: [
+            {
+                hour: 0,
+                noOfAccidents: 267
+            },
+            {
+                hour: 1,
+                noOfAccidents: 230
+            },
+            {
+                hour: 2,
+                noOfAccidents: 177
+            },
+            {
+                hour: 3,
+                noOfAccidents: 142
+            },
+            {
+                hour: 4,
+                noOfAccidents: 121
+            },
+            {
+                hour: 5,
+                noOfAccidents: 105
+            },
+            {
+                hour: 6,
+                noOfAccidents: 99
+            },
+            {
+                hour: 7,
+                noOfAccidents: 119
+            },
+            {
+                hour: 8,
+                noOfAccidents: 146
+            },
+            {
+                hour: 9,
+                noOfAccidents: 216
+            },
+            {
+                hour: 10,
+                noOfAccidents: 279
+            },
+            {
+                hour: 11,
+                noOfAccidents: 365
+            },
+            {
+                hour: 12,
+                noOfAccidents: 478
+            },
+            {
+                hour: 13,
+                noOfAccidents: 535
+            },
+            {
+                hour: 14,
+                noOfAccidents: 540
+            },
+            {
+                hour: 15,
+                noOfAccidents: 502
+            },
+            {
+                hour: 16,
+                noOfAccidents: 485
+            },
+            {
+                hour: 17,
+                noOfAccidents: 489
+            },
+            {
+                hour: 18,
+                noOfAccidents: 490
+            },
+            {
+                hour: 19,
+                noOfAccidents: 495
+            },
+            {
+                hour: 20,
+                noOfAccidents: 409
+            },
+            {
+                hour: 21,
+                noOfAccidents: 327
+            },
+            {
+                hour: 22,
+                noOfAccidents: 271
+            },
+            {
+                hour: 23,
+                noOfAccidents: 216
+            }
+        ]
+    },
+    {
+        day: "Monday",
+        hourResults: [
+            {
+                hour: 0,
+                noOfAccidents: 139
+            },
+            {
+                hour: 1,
+                noOfAccidents: 126
+            },
+            {
+                hour: 2,
+                noOfAccidents: 69
+            },
+            {
+                hour: 3,
+                noOfAccidents: 62
+            },
+            {
+                hour: 4,
+                noOfAccidents: 52
+            },
+            {
+                hour: 5,
+                noOfAccidents: 74
+            },
+            {
+                hour: 6,
+                noOfAccidents: 188
+            },
+            {
+                hour: 7,
+                noOfAccidents: 444
+            },
+            {
+                hour: 8,
+                noOfAccidents: 585
+            },
+            {
+                hour: 9,
+                noOfAccidents: 395
+            },
+            {
+                hour: 10,
+                noOfAccidents: 364
+            },
+            {
+                hour: 11,
+                noOfAccidents: 360
+            },
+            {
+                hour: 12,
+                noOfAccidents: 463
+            },
+            {
+                hour: 13,
+                noOfAccidents: 524
+            },
+            {
+                hour: 14,
+                noOfAccidents: 528
+            },
+            {
+                hour: 15,
+                noOfAccidents: 706
+            },
+            {
+                hour: 16,
+                noOfAccidents: 703
+            },
+            {
+                hour: 17,
+                noOfAccidents: 824
+            },
+            {
+                hour: 18,
+                noOfAccidents: 756
+            },
+            {
+                hour: 19,
+                noOfAccidents: 553
+            },
+            {
+                hour: 20,
+                noOfAccidents: 396
+            },
+            {
+                hour: 21,
+                noOfAccidents: 306
+            },
+            {
+                hour: 22,
+                noOfAccidents: 241
+            },
+            {
+                hour: 23,
+                noOfAccidents: 187
+            }
+        ]
+    },
+    {
+        day: "Tuesday",
+        hourResults: [
+            {
+                hour: 0,
+                noOfAccidents: 86
+            },
+            {
+                hour: 1,
+                noOfAccidents: 78
+            },
+            {
+                hour: 2,
+                noOfAccidents: 67
+            },
+            {
+                hour: 3,
+                noOfAccidents: 28
+            },
+            {
+                hour: 4,
+                noOfAccidents: 66
+            },
+            {
+                hour: 5,
+                noOfAccidents: 85
+            },
+            {
+                hour: 6,
+                noOfAccidents: 218
+            },
+            {
+                hour: 7,
+                noOfAccidents: 513
+            },
+            {
+                hour: 8,
+                noOfAccidents: 784
+            },
+            {
+                hour: 9,
+                noOfAccidents: 527
+            },
+            {
+                hour: 10,
+                noOfAccidents: 407
+            },
+            {
+                hour: 11,
+                noOfAccidents: 409
+            },
+            {
+                hour: 12,
+                noOfAccidents: 447
+            },
+            {
+                hour: 13,
+                noOfAccidents: 529
+            },
+            {
+                hour: 14,
+                noOfAccidents: 566
+            },
+            {
+                hour: 15,
+                noOfAccidents: 716
+            },
+            {
+                hour: 16,
+                noOfAccidents: 769
+            },
+            {
+                hour: 17,
+                noOfAccidents: 892
+            },
+            {
+                hour: 18,
+                noOfAccidents: 809
+            },
+            {
+                hour: 19,
+                noOfAccidents: 608
+            },
+            {
+                hour: 20,
+                noOfAccidents: 419
+            },
+            {
+                hour: 21,
+                noOfAccidents: 323
+            },
+            {
+                hour: 22,
+                noOfAccidents: 289
+            },
+            {
+                hour: 23,
+                noOfAccidents: 177
+            }
+        ]
+    },
+    {
+        day: "Wednesday",
+        hourResults: [
+            {
+                hour: 0,
+                noOfAccidents: 92
+            },
+            {
+                hour: 1,
+                noOfAccidents: 83
+            },
+            {
+                hour: 2,
+                noOfAccidents: 59
+            },
+            {
+                hour: 3,
+                noOfAccidents: 60
+            },
+            {
+                hour: 4,
+                noOfAccidents: 51
+            },
+            {
+                hour: 5,
+                noOfAccidents: 79
+            },
+            {
+                hour: 6,
+                noOfAccidents: 204
+            },
+            {
+                hour: 7,
+                noOfAccidents: 497
+            },
+            {
+                hour: 8,
+                noOfAccidents: 712
+            },
+            {
+                hour: 9,
+                noOfAccidents: 439
+            },
+            {
+                hour: 10,
+                noOfAccidents: 382
+            },
+            {
+                hour: 11,
+                noOfAccidents: 413
+            },
+            {
+                hour: 12,
+                noOfAccidents: 523
+            },
+            {
+                hour: 13,
+                noOfAccidents: 562
+            },
+            {
+                hour: 14,
+                noOfAccidents: 639
+            },
+            {
+                hour: 15,
+                noOfAccidents: 806
+            },
+            {
+                hour: 16,
+                noOfAccidents: 754
+            },
+            {
+                hour: 17,
+                noOfAccidents: 865
+            },
+            {
+                hour: 18,
+                noOfAccidents: 868
+            },
+            {
+                hour: 19,
+                noOfAccidents: 709
+            },
+            {
+                hour: 20,
+                noOfAccidents: 435
+            },
+            {
+                hour: 21,
+                noOfAccidents: 359
+            },
+            {
+                hour: 22,
+                noOfAccidents: 281
+            },
+            {
+                hour: 23,
+                noOfAccidents: 227
+            }
+        ]
+    },
+    {
+        day: "Thursday",
+        hourResults: [
+            {
+                hour: 0,
+                noOfAccidents: 125
+            },
+            {
+                hour: 1,
+                noOfAccidents: 93
+            },
+            {
+                hour: 2,
+                noOfAccidents: 67
+            },
+            {
+                hour: 3,
+                noOfAccidents: 51
+            },
+            {
+                hour: 4,
+                noOfAccidents: 47
+            },
+            {
+                hour: 5,
+                noOfAccidents: 67
+            },
+            {
+                hour: 6,
+                noOfAccidents: 211
+            },
+            {
+                hour: 7,
+                noOfAccidents: 492
+            },
+            {
+                hour: 8,
+                noOfAccidents: 771
+            },
+            {
+                hour: 9,
+                noOfAccidents: 492
+            },
+            {
+                hour: 10,
+                noOfAccidents: 372
+            },
+            {
+                hour: 11,
+                noOfAccidents: 438
+            },
+            {
+                hour: 12,
+                noOfAccidents: 534
+            },
+            {
+                hour: 13,
+                noOfAccidents: 591
+            },
+            {
+                hour: 14,
+                noOfAccidents: 583
+            },
+            {
+                hour: 15,
+                noOfAccidents: 815
+            },
+            {
+                hour: 16,
+                noOfAccidents: 798
+            },
+            {
+                hour: 17,
+                noOfAccidents: 915
+            },
+            {
+                hour: 18,
+                noOfAccidents: 881
+            },
+            {
+                hour: 19,
+                noOfAccidents: 643
+            },
+            {
+                hour: 20,
+                noOfAccidents: 495
+            },
+            {
+                hour: 21,
+                noOfAccidents: 376
+            },
+            {
+                hour: 22,
+                noOfAccidents: 336
+            },
+            {
+                hour: 23,
+                noOfAccidents: 241
+            }
+        ]
+    },
+    {
+        day: "Friday",
+        hourResults: [
+            {
+                hour: 0,
+                noOfAccidents: 154
+            },
+            {
+                hour: 1,
+                noOfAccidents: 107
+            },
+            {
+                hour: 2,
+                noOfAccidents: 66
+            },
+            {
+                hour: 3,
+                noOfAccidents: 66
+            },
+            {
+                hour: 4,
+                noOfAccidents: 68
+            },
+            {
+                hour: 5,
+                noOfAccidents: 79
+            },
+            {
+                hour: 6,
+                noOfAccidents: 189
+            },
+            {
+                hour: 7,
+                noOfAccidents: 466
+            },
+            {
+                hour: 8,
+                noOfAccidents: 641
+            },
+            {
+                hour: 9,
+                noOfAccidents: 441
+            },
+            {
+                hour: 10,
+                noOfAccidents: 413
+            },
+            {
+                hour: 11,
+                noOfAccidents: 470
+            },
+            {
+                hour: 12,
+                noOfAccidents: 582
+            },
+            {
+                hour: 13,
+                noOfAccidents: 644
+            },
+            {
+                hour: 14,
+                noOfAccidents: 717
+            },
+            {
+                hour: 15,
+                noOfAccidents: 837
+            },
+            {
+                hour: 16,
+                noOfAccidents: 859
+            },
+            {
+                hour: 17,
+                noOfAccidents: 961
+            },
+            {
+                hour: 18,
+                noOfAccidents: 943
+            },
+            {
+                hour: 19,
+                noOfAccidents: 821
+            },
+            {
+                hour: 20,
+                noOfAccidents: 634
+            },
+            {
+                hour: 21,
+                noOfAccidents: 459
+            },
+            {
+                hour: 22,
+                noOfAccidents: 389
+            },
+            {
+                hour: 23,
+                noOfAccidents: 309
+            }
+        ]
+    },
+    {
+        day: "Saturday",
+        hourResults: [
+            {
+                hour: 0,
+                noOfAccidents: 251
+            },
+            {
+                hour: 1,
+                noOfAccidents: 235
+            },
+            {
+                hour: 2,
+                noOfAccidents: 169
+            },
+            {
+                hour: 3,
+                noOfAccidents: 126
+            },
+            {
+                hour: 4,
+                noOfAccidents: 107
+            },
+            {
+                hour: 5,
+                noOfAccidents: 106
+            },
+            {
+                hour: 6,
+                noOfAccidents: 117
+            },
+            {
+                hour: 7,
+                noOfAccidents: 161
+            },
+            {
+                hour: 8,
+                noOfAccidents: 212
+            },
+            {
+                hour: 9,
+                noOfAccidents: 290
+            },
+            {
+                hour: 10,
+                noOfAccidents: 384
+            },
+            {
+                hour: 11,
+                noOfAccidents: 462
+            },
+            {
+                hour: 12,
+                noOfAccidents: 614
+            },
+            {
+                hour: 13,
+                noOfAccidents: 639
+            },
+            {
+                hour: 14,
+                noOfAccidents: 660
+            },
+            {
+                hour: 15,
+                noOfAccidents: 562
+            },
+            {
+                hour: 16,
+                noOfAccidents: 611
+            },
+            {
+                hour: 17,
+                noOfAccidents: 663
+            },
+            {
+                hour: 18,
+                noOfAccidents: 684
+            },
+            {
+                hour: 19,
+                noOfAccidents: 648
+            },
+            {
+                hour: 20,
+                noOfAccidents: 529
+            },
+            {
+                hour: 21,
+                noOfAccidents: 447
+            },
+            {
+                hour: 22,
+                noOfAccidents: 405
+            },
+            {
+                hour: 23,
+                noOfAccidents: 364
+            }
+        ]
+    }
+]
